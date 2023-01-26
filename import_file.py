ed222e259599621a1b423342f60f2c4587445a8f
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2023-01-26 16:19:09 +0300

    add import_file.py

---
+++ import_file.py
@@ -0,0 +1,10 @@
+# модуль импорта данных
+def import_data(data, sep=None):
+    with open('phone_2.txt', 'a+', encoding='utf-8') as file:
+        if sep == None:
+            for i in data:
+                file.write(f"{i}\n")
+            file.write(f"\n")
+        else:
+            file.write(sep.join(data))
+            file.write(f"\n")
